c9e17971b9f8f387b11cad00f6f8f7fae0120b8b
Julio Tain Sueiras <juliosueiras@gmail.com>
2017-05-25 21:21:53 -0400

    Added js files for now

---
+++ index.ios.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var AutoComplete = (function () {
+    function AutoComplete() {
+    }
+    return AutoComplete;
+}());
+exports.AutoComplete = AutoComplete;
